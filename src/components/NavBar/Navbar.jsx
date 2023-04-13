355b52b1ed711722723c8dfd612439bf34eac23a
Denise Esperon <denusz@live.com.ar>
2023-04-13 03:08:23 -0300

    Carrito y logo

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,15 +1,21 @@
+import CartWidget from "../CartWidget/CartWidget";
 import styles from "./Navbar.module.css";
-
+import imgLogo from"../../assets/images/jade sin fondo letras blancas.png"
 export const Navbar = () => {
     return(
         <div className={styles.containerNavbar}>
-        <ul>
-        <li>Home</li>
+        <img 
+        src={imgLogo} 
+        alt="Este es el logo de la página" />
+        <ul style={{display: "flex", gap: "30px"}}>
+        <li>Serum</li>
         <li>Hidratacion</li>
         <li>Antiage</li>
         <li>Limpieza</li>
-        <li>Todas</li>
+        <li>Tonicos</li>
         </ul>
+        <CartWidget/>
+
         </div>
     );
 }
